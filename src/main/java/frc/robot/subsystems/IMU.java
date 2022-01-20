90b53db46f435b2073ed34ae8fbd4e4484bbd817
I v a n <92064541+FridgeRaider23@users.noreply.github.com>
2022-01-19 19:54:10 -0500

    updated imu
    i am you
    parmesan

--- src/main/java/frc/robot/subsystems/IMU.java
+++ src/main/java/frc/robot/subsystems/IMU.java
@@ -4,40 +4,50 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
 
-public class IMU extends SubsystemBase {
+public class IMU extends SubsystemBase 
+{
   // https://docs.ctre-phoenix.com/en/stable/ch11_BringUpPigeon.html
   // https://store.ctr-electronics.com/content/api/java/html/classcom_1_1ctre_1_1phoenix_1_1sensors_1_1_pigeon_i_m_u.html
   // reference: store.ctr-electronics.com/content/api/java/html/index.html
   // reference: 2021 robot drivetrain subsystem (github)
 
-  // TODO: Create Pigeon IMU Object (WPI_PigeonIMU)
+  private WPI_PigeonIMU pigeonIMU;
+  private double heading;
 
   /** Creates a new IMU. */
-  public IMU() {
-    // TODO: Calibrate (2021 code)
+  public IMU() 
+  {
+    pigeonIMU = new WPI_PigeonIMU(9);
+    pigeonIMU.setFusedHeading(0);
   }
 
   @Override
-  public void periodic() {
+  public void periodic() 
+  {
     // This method will be called once per scheduler run
+    heading = pigeonIMU.getFusedHeading();
 
-    // TODO: Read the gyro data (getFusedHeading)
-
-    // TODO: Display data to shuffleboard
+    SmartDashboard.putNumber("pigeonIMUData", heading); 
   }
 
   // Access gyro data read in periodic
-  public double getAngleDegrees() {
-    return 0.0;
+  public double getAngleDegrees() 
+  {
+    return heading;
   }
 
-  public double getAngleRadians() {
-    return 0.0;
+  public double getAngleRadians() 
+  {
+    return Math.toRadians(heading);
   }
 
-  public void resetHeading() {
-    // TODO: Set heading to 0 (setFusedHeading)
+  public void resetHeading() 
+  {
+    pigeonIMU.setFusedHeading(0);
+    heading = 0;
   }
 }
