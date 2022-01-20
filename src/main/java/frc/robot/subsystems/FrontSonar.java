781f4e2dd9b3804e015f84c48a7b6d47802c51c7
Oli <24oliviao@gmail.com>
2022-01-19 20:50:46 -0500

    Added basic Sonar Range code + shuffleboard, and removed extra spaces

--- src/main/java/frc/robot/subsystems/FrontSonar.java
+++ src/main/java/frc/robot/subsystems/FrontSonar.java
@@ -4,18 +4,27 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FrontSonar extends SubsystemBase {
   // https://www.maxbotix.com/ultrasonic_sensors/mb1043.htm
   // https://docs.wpilib.org/en/stable/docs/software/hardware-apis/sensors/analog-inputs-software.html
 
+  AnalogInput analog = new AnalogInput(0);
+  double inputVol = -1;
+
   /** Creates a new FrontSonar. */
-  public FrontSonar() {}
+  public FrontSonar() {
+    
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    inputVol = analog.getVoltage();
+    SmartDashboard.putNumber("Sonar Range", inputVol);
 
     // TODO: Read the sensor and save to a variable
     // TODO: Look into filtering
@@ -24,6 +33,6 @@ public class FrontSonar extends SubsystemBase {
   // sensorNumber determines which sensor to read from
   // If invalid, return -1, otherwise range in meters
   public double getRange(int sensorNumber) {
-    return 0.0;
+    return inputVol;
   }
 }
