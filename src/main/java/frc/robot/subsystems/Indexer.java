62379d9ade59ef414369d929a28e45b5416cc6b2
Oli <24oliviao@gmail.com>
2022-02-05 15:01:48 -0500

    Updated Indexer from the Shooter branch (no more ToF sensor and new methods)

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -4,59 +4,24 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DutyCycle;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 
 public class Indexer extends SubsystemBase {
-
-  private DigitalInput tofInput;
-  private DutyCycle tofDutyCycleInput;
-
-  private double tofFreq;
-  private double tofDutyCycle;
-  private double tofRange;
-  private boolean ballHeld;
   private double motorVelocity;
-
-  private final double tofScaleFactor = 0.004 / (1e-6);
 
   /** Creates a new Indexer. */
   public Indexer() {
-    tofInput = new DigitalInput(0);
-    tofDutyCycleInput = new DutyCycle(tofInput);
-    tofFreq = 0;
-    tofRange = 0;
-    ballHeld = false;
     motorVelocity = 0.0;
   }
 
   @Override
   public void periodic() {
-    tofFreq = tofDutyCycleInput.getFrequency();
-    tofDutyCycle = tofDutyCycleInput.getOutput();
-    tofRange = tofScaleFactor * (tofDutyCycle / tofFreq - 0.001);
-    SmartDashboard.putNumber("TOF Frequency", tofFreq);
-    SmartDashboard.putNumber("TOF Duty Cycle", tofDutyCycle);
-    SmartDashboard.putNumber("TOF Time", tofDutyCycle / tofFreq);
-    SmartDashboard.putNumber("TOF Range", tofRange);
-
     if (motorVelocity > 0.0) {
       Robot.getBling().setSlot(1, 60, 168, 50);
     } else {
       Robot.getBling().setSlot(1, 168, 50, 50);
     }
-  }
-
-  public double getRange() {
-    return tofRange;
-  }
-
-  public boolean isBallHeld() {
-    return ballHeld;
-    // ^ This returns true if the time of flight sensor detects that the ball is in the wheels.
   }
 
   public void setWheelVelocity(double velocity) {
@@ -65,6 +30,22 @@ public class Indexer extends SubsystemBase {
 
   public double getWheelVelocity() {
     return 0.0;
+  }
+
+  public void isCurrentCargoColored(){
+
+  }
+
+  public void isNextCargoColored(){
+
+  }
+
+  public void getNumCargo(){
+
+  }
+
+  public void getCargoPositions(){
+    
   }
 
   /*
